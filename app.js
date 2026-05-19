7e74af19ab515b1ad5d132d8e12d98c6926e7e58
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-05-19 15:56:48 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+async function validarLogin() {
+    const u = document.getElementById('usuario').value;
+    const p = document.getElementById('contrasena').value;
+    const m = document.getElementById('mensaje');
+    
+    m.innerText = "Conectando...";
+
+    try {
+        const respuesta = await fetch("/api/login", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ usuario: u, contrasena: p })
+        });
+        const data = await respuesta.json();
+        
+        if (data.success) {
+            m.innerText = "¡Bienvenido!";
+            m.style.color = "green";
+        } else {
+            m.innerText = data.error;
+            m.style.color = "red";
+        }
+    } catch (e) {
+        m.innerText = "Error de red.";
+        m.style.color = "red";
+    }
+}
